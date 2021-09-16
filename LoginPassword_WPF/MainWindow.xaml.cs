61c9917d9312fd3ce846095ddaf8eba67130f59e
Курицын Алексей <al.curitsin@gmail.com>
2021-09-16 10:38:17 +0500

    MultiDataTrigger --do not working.

---
+++ LoginPassword_WPF/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LoginPassword_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPasswordChaged(object sender, RoutedEventArgs e)
+        {
+            PasswordBox pb = sender as PasswordBox;
+            pb.Tag = (!string.IsNullOrEmpty(pb.Password)).ToString();
+        }
+    }
+}
